3e9e3aa744ac77e399f0148a1419fe7c48022743
hyubee <hyubee@hyubees-MacBook-Pro.local>
2021-11-18 14:34:51 +0900

    Feat : .env setting & Mongoose Setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedToPology: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
