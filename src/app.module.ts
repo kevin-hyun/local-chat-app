357b967c96c0986c56fd0e1d17e44965b539af2d
hyubee <hyubee@hyubees-MacBook-Pro.local>
2021-11-18 16:21:23 +0900

    Feat : Setting for MongoDB
    apply MongoUrI to env var with  .env
    install package for mongoDB connection
    create User schema and import module for it

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    UsersModule,
     MongooseModule.forRoot(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedToPology: true,
---
+++ src/users/users.module.ts
@@ -0,0 +1,15 @@
+import { User, UserSchema } from './schemas/user.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
+    ]),
+  ],
+})
+export class UsersModule {}
